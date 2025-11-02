fb42271bf6a9d8da3ed43526dda02971a1b25c33
Luciano Garin Iriarte <luchiga19@outlook.com>
2025-11-02 02:32:25 -0500

    doc: added docstring documentation of add function

--- operators.py
+++ operators.py
@@ -1,4 +1,14 @@
 def add(a,b):
+    """
+    This function does a basic addition between two floats
+
+    Args:
+        a (float): first float to add
+        b (float): second float to add
+
+    Returns:
+        float: result of the addition
+    """
     return a + b
 
 def subtract(a,b):
