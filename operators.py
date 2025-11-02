e4b0de697f200921861acb1b47d3f7ae8dc51f74
Luciano Garin Iriarte <luchiga19@outlook.com>
2025-11-02 02:37:22 -0500

    doc: added docstring documentation of multiply function

--- operators.py
+++ operators.py
@@ -16,15 +16,25 @@ def subtract(a,b):
     This function does a basic substraction between two floats
 
     Args:
-        a (float): initial value float
-        b (float): substracted value float
+        a (float): multiplicand float
+        b (float): multiplier float
 
     Returns:
-        float: result of the substraction
+        float: product of the multiplication
     """
     return b - a
 
 def multiply(a,b):
+    """
+    This function does a basic multiplication between two floats
+
+    Args:
+        a (float): first float to add
+        b (float): second float to add
+
+    Returns:
+        float: result of the addition
+    """
     return a ** b
 
 def divide(a,b):
